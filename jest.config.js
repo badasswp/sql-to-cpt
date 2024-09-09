bf328638726340f9dbb59edec7cc1d89ecde2ca7
badasswp <badasswpdev@gmail.com>
2024-09-09 12:18:44 +0100

    chore: add jest config files

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const baseConfig = require('@wordpress/scripts/config/jest-unit.config.js');
+
+module.exports = {
+  ...baseConfig,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    './jest.setup.js'
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    'uuid': require.resolve('uuid'),
+  },
+};
---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect.js';
+import '@wordpress/jest-preset-default/scripts/setup-globals';
