09b982f03c5b090c8aa27677b59f5fbc700a9133
badasswp <badasswpdev@gmail.com>
2024-09-09 12:32:49 +0100

    chore: add plugin singleton instance

---
+++ inc/Plugin.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plugin Class.
+ *
+ * This class is responsible for establishing
+ * and managing the singleton instance of the plugin.
+ *
+ * @package SQLToCPT
+ */
+
+namespace SQLToCPT;
+
+use SQLToCPT\Core\Container;
+
+class Plugin {
+	/**
+	 * Plugin instance.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var Plugin
+	 */
+	protected static $instance;
+
+	/**
+	 * Get Instance.
+	 *
+	 * Return singeleton instance for Plugin.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return Plugin
+	 */
+	public static function get_instance(): Plugin {
+		if ( null === static::$instance ) {
+			static::$instance = new self();
+		}
+
+		return static::$instance;
+	}
+
+	/**
+	 * Bind to WP.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function run(): void {
+		( new Container() )->register();
+	}
+}
