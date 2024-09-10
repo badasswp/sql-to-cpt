db387e4f6292f51fd6819b611800dad61116c9bc
badasswp <badasswpdev@gmail.com>
2024-09-10 10:32:49 +0100

    chore: add Container class

---
+++ inc/Core/Container.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Container class.
+ *
+ * This class is responsible for registering the
+ * plugin services.
+ *
+ * @package SqlToCpt
+ */
+
+namespace SqlToCpt\Core;
+
+use SqlToCpt\Services\Admin;
+use SqlToCpt\Services\Routes;
+use SqlToCpt\Interfaces\Kernel;
+
+class Container implements Kernel {
+	/**
+	 * Services.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var mixed[]
+	 */
+	public static array $services = [];
+
+	/**
+	 * Prepare Singletons.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		static::$services = [
+			Admin::class,
+			Routes::class,
+		];
+	}
+
+	/**
+	 * Register Service.
+	 *
+	 * Establish singleton version for each Service
+	 * concrete class.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		foreach ( static::$services as $service ) {
+			( $service::get_instance() )->register();
+		}
+	}
+}
