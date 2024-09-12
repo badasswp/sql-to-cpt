a2900b1b4b273c7e532739956fa34b5136a0744e
badasswp <badasswpdev@gmail.com>
2024-09-12 11:41:54 +0100

    feat: set up method for upload

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,33 @@
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+
+import { getModalParams } from '../utils';
+import '../styles/app.scss';
+
 const App = () => {
+  const handleUpload = (e) => {
+    e.preventDefault();
+    const wpMediaModal = wp.media( getModalParams() );
+
+    const doImport = () => {
+      const attachment = wpMediaModal.state().get('selection').first().toJSON();
+      if ( 'application/json' !== attachment.mime ) {
+        console.log( 'Selected file:', attachment );
+      }
+    };
+
+    wpMediaModal.on( 'select', doImport ).open();
+  };
+
   return (
-    <h1>Hello World</h1>
+    <main>
+      <Button
+        variant="primary"
+        onClick={handleUpload}
+      >
+        { __('Import SQL File', 'sql-to-cpt') }
+      </Button>
+    </main>
   )
 }
 
