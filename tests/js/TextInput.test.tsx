72f609498ca08b68ef662d3e6f5ae6752233dbc2
badasswp <badasswpdev@gmail.com>
2024-10-22 12:19:28 +0100

    test: add tests for TextInput component

---
+++ tests/js/TextInput.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import TextInput from '../../src/components/TextInput';
+
+describe( 'TextInput', () => {
+  it( 'renders the component with correct text', () => {
+    const { container } = render( <TextInput name="post_title" /> );
+
+    // Expect Component to look like so:
+    expect( container.innerHTML ).toBe(
+      `<p><input type="text" value="post_title"></p>`
+    );
+
+    // Assert the input is rendered and is disabled
+    const input = screen.getByRole( 'textbox' );
+    expect( input ).toHaveValue( 'post_title' );
+  } );
+} );
