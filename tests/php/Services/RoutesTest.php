cd04f41db35f70be333a2c3ca1cdbdda7874f813
badasswp <badasswpdev@gmail.com>
2024-12-15 13:13:07 +0100

    test: service contains routes

--- tests/php/Services/RoutesTest.php
+++ tests/php/Services/RoutesTest.php
@@ -4,11 +4,16 @@ namespace SqlToCpt\Tests\Services;
 
 use Mockery;
 use WP_Mock\Tools\TestCase;
+
+use SqlToCpt\Routes\Parse;
+use SqlToCpt\Routes\Import;
 use SqlToCpt\Services\Routes;
+use SqlToCpt\Abstracts\Route;
 use SqlToCpt\Abstracts\Service;
 
 /**
  * @covers \SqlToCpt\Services\Routes::register
+ * @covers \SqlToCpt\Services\Routes::register_rest_routes
  * @covers \SqlToCpt\Services\Routes::__construct
  */
 class RoutesTest extends TestCase {
@@ -28,6 +33,18 @@ class RoutesTest extends TestCase {
 		\WP_Mock::expectActionAdded( 'rest_api_init', [ $this->routes, 'register_rest_routes' ] );
 
 		$this->routes->register();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_service_contains_routes_at_instantiation() {
+		$this->assertSame(
+			$this->routes->routes,
+			[
+				Parse::class,
+				Import::class,
+			]
+		);
 
 		$this->assertConditionsMet();
 	}
