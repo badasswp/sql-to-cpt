6a197e56d7ec6f4abe119beeef5d7449e22befea
badasswp <badasswpdev@gmail.com>
2024-12-15 13:51:00 +0100

    test: set up test template for Post service

---
+++ tests/php/Services/PostTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SqlToCpt\Tests\Services;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+
+use SqlToCpt\Services\Post;
+use SqlToCpt\Abstracts\Service;
+use SqlToCpt\Core\Post as CPT;
+
+/**
+ * @covers \SqlToCpt\Services\Post::register
+ * @covers \SqlToCpt\Services\Post::register_post_types
+ * @covers \SqlToCpt\Services\Post::__construct
+ */
+class PostTest extends TestCase {
+	public Post $post;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		\WP_Mock::userFunction( 'get_option' )
+			->with( 'sql_to_cpt', [] )
+			->andReturn(
+				[
+					'cpts' => [
+						'student',
+						'department'
+					]
+				]
+			);
+
+		\WP_Mock::expectFilter(
+			'sqlt_cpt_post_types',
+			[
+				'student',
+				'department'
+			]
+		);
+
+		$this->post = new Post();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+}
