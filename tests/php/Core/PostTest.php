2089f4a6a77d876e91352db010ead9c1bb943cca
badasswp <badasswpdev@gmail.com>
2024-12-16 20:45:38 +0100

    test: add tests for labels

--- tests/php/Core/PostTest.php
+++ tests/php/Core/PostTest.php
@@ -22,4 +22,16 @@ class PostTest extends TestCase {
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
 	}
+
+	public function test_get_name() {
+		$this->assertSame( $this->post->get_name(), 'student' );
+	}
+
+	public function test_get_singular_label() {
+		$this->assertSame( $this->post->get_singular_label(), 'Student' );
+	}
+
+	public function test_get_plural_label() {
+		$this->assertSame( $this->post->get_plural_label(), 'Students' );
+	}
 }
