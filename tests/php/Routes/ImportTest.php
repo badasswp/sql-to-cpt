577a0b472016931446bed3627d9a9041b9fae736
badasswp <badasswpdev@gmail.com>
2024-12-21 16:11:06 +0100

    test: response passes correctly

--- tests/php/Routes/ImportTest.php
+++ tests/php/Routes/ImportTest.php
@@ -120,4 +120,44 @@ class ImportTest extends TestCase {
 		$this->assertInstanceOf( \WP_Error::class, $response );
 		$this->assertConditionsMet();
 	}
+
+	public function test_response_passes_correctly() {
+		$import = Mockery::mock( Import::class )->makePartial();
+		$import->shouldAllowMockingProtectedMethods();
+
+		$request = Mockery::mock( \WP_REST_Request::class )->makePartial();
+		$request->shouldAllowMockingProtectedMethods();
+
+		$request->shouldReceive( 'get_json_params' )
+			->andReturn(
+				[
+					'tableName'    => 'student',
+					'tableColumns' => [ 'id', 'name', 'age', 'sex', 'email_address', 'date_created' ],
+					'tableRows'    => [
+						[ 1, 'John Doe', 37, 'M', 'john@doe.com', '00:00:00' ],
+					],
+				]
+			);
+
+		$import->request = $request;
+
+		$import->shouldReceive( 'get_response' )
+			->andReturn(
+				sprintf(
+					'https://example.com/wp-admin/edit.php?%s',
+					http_build_query(
+						[
+							'post_type' => 'post',
+						]
+					)
+				)
+			);
+
+		Mockery::mock( \WP_REST_Response::class )->makePartial();
+
+		$response = $import->response();
+
+		$this->assertInstanceOf( \WP_REST_Response::class, $response );
+		$this->assertConditionsMet();
+	}
 }
