744d5c539c6fd8ecef271a43a1d23922c9ab368a
badasswp <badasswpdev@gmail.com>
2024-12-21 03:33:05 +0100

    test: set up scafffolding for Import class tests

---
+++ tests/php/Routes/ImportTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SqlToCpt\Tests\Routes;
+
+use Mockery;
+use WP_Mock\Tools\TestCase;
+use SqlToCpt\Routes\Import;
+use SqlToCpt\Abstracts\Service;
+
+/**
+ * @covers \SqlToCpt\Routes\Import::register
+ */
+class ImportTest extends TestCase {
+	public Import $import;
+
+	public function setUp(): void {
+		\WP_Mock::setUp();
+
+		$this->import = new Import();
+	}
+
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+}
