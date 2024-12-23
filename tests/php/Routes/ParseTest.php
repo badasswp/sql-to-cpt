bfb01a654d453cfc1fccadacfdf9564734785d48
badasswp <badasswpdev@gmail.com>
2024-12-23 06:26:10 +0100

    test: add tests for Parse response

--- tests/php/Routes/ParseTest.php
+++ tests/php/Routes/ParseTest.php
@@ -53,6 +53,43 @@ class ParseTest extends TestCase {
 		$this->assertConditionsMet();
 	}
 
+	public function test_response_bails_out_if_file_is_not_sql() {
+		$txt_file = __DIR__ . '/import.txt';
+		$this->create_mock_file( $txt_file );
+
+		$parse = Mockery::mock( Parse::class )->makePartial();
+		$parse->shouldAllowMockingProtectedMethods();
+
+		$request = Mockery::mock( \WP_REST_Request::class )->makePartial();
+		$request->shouldAllowMockingProtectedMethods();
+
+		$request->shouldReceive( 'get_json_params' )
+			->andReturn(
+				[
+					'id' => 1,
+				]
+			);
+
+		$parse->request = $request;
+
+		\WP_Mock::userFunction( 'get_attached_file' )
+			->with( 1 )
+			->andReturn( $txt_file );
+
+		$parse->shouldReceive( 'is_sql' )
+			->with( $txt_file )
+			->andReturnUsing(
+				function ( $path ) {
+					return 'sql' === pathinfo( $path, PATHINFO_EXTENSION );
+				}
+			);
+
+		$this->assertInstanceOf( \WP_Error::class, $parse->response() );
+		$this->assertConditionsMet();
+
+		$this->destroy_mock_file( $txt_file );
+	}
+
 	public function create_mock_file( $mock_file ) {
 		file_put_contents( $mock_file, 'INSERT INTO `student` (`id`, `name`, `age`, `sex`, `email_address`, `date_created`) VALUES', FILE_APPEND );
 
