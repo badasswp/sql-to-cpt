906e743090dcaa0b076939ddc3015092f7bd72ac
badasswp <badasswpdev@gmail.com>
2025-01-08 11:58:30 +0100

    test: add tests for admin page

--- tests/php/Services/AdminTest.php
+++ tests/php/Services/AdminTest.php
@@ -9,6 +9,8 @@ use SqlToCpt\Abstracts\Service;
 
 /**
  * @covers \SqlToCpt\Services\Admin::register
+ * @covers \SqlToCpt\Services\Admin::register_admin_menu
+ * @covers \SqlToCpt\Services\Admin::register_admin_page
  */
 class AdminTest extends TestCase {
 	public Admin $admin;
@@ -52,6 +54,27 @@ class AdminTest extends TestCase {
 			->andReturn( null );
 
 		$this->admin->register_admin_menu();
+
+		$this->assertConditionsMet();
+	}
+
+	public function test_register_admin_page() {
+		\WP_Mock::userFunction( 'esc_html__' )
+			->andReturnUsing(
+				function ( $arg ) {
+					return $arg;
+				}
+			);
+
+		$this->expectOutputString(
+			'<section class="wrap">
+				<h1>SQL to CPT</h1>
+				<p>Import & Convert SQL files to Custom Post Types (CPT).</p>
+				<div id="sql-to-cpt"></div>
+			</section>',
+		);
+
+		$this->admin->register_admin_page();
 
 		$this->assertConditionsMet();
 	}
