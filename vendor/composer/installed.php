49ec548d0f7070b63c8a885c0b7bd4f5c47b13ce
badasswp <badasswpdev@gmail.com>
2025-02-04 16:13:02 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/sql-to-cpt',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '4eca24c9351ceef229535d641b2013c007c64d7a',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/sql-to-cpt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '4eca24c9351ceef229535d641b2013c007c64d7a',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
