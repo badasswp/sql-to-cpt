a9f12893ca75004e0773aee9b2c8ede70f6fd688
badasswp <badasswpdev@gmail.com>
2025-02-09 17:00:26 +0100

    test: add tests for ProgressBar component

--- tests/js/ProgressBar.test.tsx
+++ tests/js/ProgressBar.test.tsx
@@ -6,22 +6,22 @@ import ProgressBar from '../../src/components/ProgressBar';
 
 describe( 'ProgressBar', () => {
   it( 'renders the Progress Bar', () => {
-    const { container } = render( <ProgressBar isLoading={true} progress={75} /> );
+    const { container } = render( <ProgressBar isLoading={true} /> );
 
     // Expect Component to look like so:
     expect( container.innerHTML ).toBe(
-      `<div class="sqlt-cpt-progress-bar" role="progressbar"><div><div style="width: 75%;"></div></div><p>75%</p></div>`
+      `<div class="sqlt-cpt-progress-bar" role="progressbar"><div><div style="width: 0%;"></div></div><p>0%</p></div>`
     );
 
     // Assert the ProgressBar is rendered and is disabled.
     const progressBar = screen.getByRole( 'progressbar' );
     expect( progressBar ).toBeInTheDocument();
     expect( progressBar ).toBeInstanceOf( HTMLDivElement );
-    expect( progressBar ).toContainHTML( '<div><div style="width: 75%;"></div></div><p>75%</p>' );
+    expect( progressBar ).toContainHTML( '<div><div style="width: 0%;"></div></div><p>0%</p>' );
   } );
 
   it( 'DOES NOT render the Progress Bar', () => {
-    const { container } = render( <ProgressBar isLoading={false} progress={75} /> );
+    const { container } = render( <ProgressBar isLoading={false} /> );
 
     // Expect Component to look like so:
     expect( container.innerHTML ).toBe( `` );
