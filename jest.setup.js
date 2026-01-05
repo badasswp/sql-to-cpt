2ed198e08df36a737490eed39732db198b65d1cc
badasswp <badasswpdev@gmail.com>
2026-01-05 04:46:46 +0000

    push to master-built

--- jest.setup.js
+++
@@ -1,3 +0,0 @@
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect.js';
-import '@wordpress/jest-preset-default/scripts/setup-globals';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/sql-to-cpt',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b4e764b9abb0869b3e3be20e3dc9ae3a40427823',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/sql-to-cpt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b4e764b9abb0869b3e3be20e3dc9ae3a40427823',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
