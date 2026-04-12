fe8688bd289f7040d798acc2ffcad65d0745ae16
badasswp <badasswpdev@gmail.com>
2026-04-12 13:46:19 +0100

    test: add e2e tests files

--- tests/e2e/basic.spec.js
+++ tests/e2e/basic.spec.js
@@ -1,367 +1,72 @@
 import { test, expect } from '@wordpress/e2e-test-utils-playwright';
+import path from 'path';
 
-export async function createNewPost( page ) {
-	await page.goto( '/wp-admin/post-new.php' );
-	await page.waitForSelector( '.edit-post-layout' );
+export async function openPluginPage( page ) {
+	await page.goto( '/wp-admin/admin.php?page=sql-to-cpt' );
+	await page.waitForSelector( '#sql-to-cpt' );
 }
 
-test.describe( 'Search & Replace', () => {
+test.describe( 'SQL to CPT', () => {
 	test.beforeEach( async ( { page } ) => {
-		createNewPost( page );
+		openPluginPage( page );
 	} );
 
-	test( 'it displays the plugin icon', async ( { page } ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
+	test( 'it displays the plugin page', async ( { page } ) => {
+		await expect(
+			page.getByRole( 'heading', { name: 'SQL to CPT' } )
+		).toBeVisible();
+		await expect(
+			page.getByText(
+				'Import & Convert SQL files to Custom Post Types (CPT)'
+			)
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'button', { name: 'Upload SQL File' } )
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'button', { name: 'Purge CPT' } )
+		).toBeVisible();
+	} );
 
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
+	test( 'it should open WP media modal', async ( { page } ) => {
+		const uploadButton = page.getByRole( 'button', {
+			name: 'Upload SQL File',
+		} );
+		await expect( uploadButton ).toBeVisible();
 
-		const pluginIcon = page.getByRole( 'button', {
-			name: 'Search & Replace',
+		await uploadButton.click();
+
+		await expect(
+			page.getByRole( 'heading', { name: 'Select SQL File' } )
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'tab', { name: 'Upload files' } )
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'tab', { name: 'Media Library' } )
+		).toBeVisible();
+		await expect(
+			page.getByRole( 'button', { name: 'Use SQL' } )
+		).toBeVisible();
+	} );
+
+	test( 'it should upload and import a SQL file', async ( { page } ) => {
+		const uploadButton = page.getByRole( 'button', {
+			name: 'Upload SQL File',
 		} );
 
-		await expect( pluginIcon ).toBeVisible();
-	} );
+		await uploadButton.click();
 
-	test( 'it displays modal on icon click', async ( { page } ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
+		const fileInput = page.locator( 'input[type="file"]' );
+		const filePath = path.resolve( __dirname, 'fixtures/students.sql' );
 
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
+		await fileInput.setInputFiles( filePath );
 
-		// Click Plugin icon.
-		await expect(
-			page.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-		).toBeVisible();
-		await page
-			.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-			.click();
+		const useSqlButton = page.getByRole( 'button', { name: 'Use SQL' } );
+		await useSqlButton.waitFor( { state: 'visible' } );
 
-		// Expect to see modal heading, label & form controls.
-		await expect(
-			page.getByRole( 'heading', { name: 'Search & Replace' } )
-		).toBeVisible();
+		await useSqlButton.click();
 
-		// The Search label & textfield.
-		await expect(
-			page
-				.locator( '#search-replace-modal__text-group' )
-				.getByText( 'Search' )
-		).toBeVisible();
-		await expect(
-			page.getByRole( 'textbox', { name: 'Search' } )
-		).toBeVisible();
-
-		// The Replace label & textfield.
-		await expect(
-			page
-				.locator( '#search-replace-modal__text-group' )
-				.getByText( 'Replace' )
-		).toBeVisible();
-		await expect(
-			page.getByRole( 'textbox', { name: 'Replace' } )
-		).toBeVisible();
-
-		// The checkboxes.
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Match case' } )
-		).toBeVisible();
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Use regular expression' } )
-		).toBeVisible();
-
-		// The buttons.
-		await expect(
-			page.getByRole( 'button', { name: 'Replace' } )
-		).toBeVisible();
-		await expect(
-			page.getByRole( 'button', { name: 'Done' } )
-		).toBeVisible();
-	} );
-
-	test( 'it matches text within the body content', async ( { page } ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
-
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
-
-		// Populate post title & content.
-		const postTitle = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'textbox', { name: 'Add title' } );
-		const postContent = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'document', { name: 'Empty block; start writing or' } );
-		const blockEditor = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'button', { name: 'Add default block' } );
-
-		await expect( postTitle ).toBeVisible();
-		await postTitle.fill( 'Lorem ipsum dolor sit amet...' );
-
-		await expect( blockEditor ).toBeVisible();
-		await blockEditor.click();
-
-		await expect( postContent ).toBeVisible();
-		await postContent.fill(
-			'Lorem ipsum dolor sit amet, consectetur adipiscing ipsum elit. Duis vestibulum at nulla vitae rutrum. Nunc purus nulla, tincidunt sed ipsum turpis in, ullamcorper commodo libero.'
-		);
-
-		// Click Plugin icon.
-		await expect(
-			page.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-		).toBeVisible();
-		await page
-			.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-			.click();
-
-		// Type in word in Search field.
-		await expect(
-			page.getByRole( 'textbox', { name: 'Search' } )
-		).toBeVisible();
-		await page.getByRole( 'textbox', { name: 'Search' } ).fill( 'ipsum' );
-
-		// Match word specific number of times.
-		await expect( page.getByText( 'item(s) found.' ) ).toBeVisible();
-		await expect( page.getByText( 'item(s) found.' ) ).toHaveText(
-			'3 item(s) found.'
-		);
-	} );
-
-	test( 'it performs search and replace for text', async ( { page } ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
-
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
-
-		// Populate post title & content.
-		const postTitle = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'textbox', { name: 'Add title' } );
-		const postContent = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'document', { name: 'Empty block; start writing or' } );
-		const blockEditor = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'button', { name: 'Add default block' } );
-
-		await expect( postTitle ).toBeVisible();
-		await postTitle.fill( 'Lorem ipsum dolor sit amet...' );
-
-		await expect( blockEditor ).toBeVisible();
-		await blockEditor.click();
-
-		await expect( postContent ).toBeVisible();
-		await postContent.fill(
-			'Lorem ipsum dolor sit amet, consectetur adipiscing ipsum elit. Duis vestibulum at nulla vitae rutrum. Nunc purus nulla, tincidunt sed ipsum turpis in, ullamcorper commodo libero.'
-		);
-
-		// Click Plugin icon.
-		await expect(
-			page.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-		).toBeVisible();
-		await page
-			.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-			.click();
-
-		// Type in word in Search field.
-		await expect(
-			page.getByRole( 'textbox', { name: 'Search' } )
-		).toBeVisible();
-		await page.getByRole( 'textbox', { name: 'Search' } ).fill( 'ipsum' );
-
-		// Type in word in Replace field.
-		await expect(
-			page.getByRole( 'textbox', { name: 'Replace' } )
-		).toBeVisible();
-		await page.getByRole( 'textbox', { name: 'Replace' } ).fill( 'hello' );
-
-		// Replace text.
-		await expect(
-			page.getByRole( 'button', { name: 'Replace' } )
-		).toBeVisible();
-		await page.getByRole( 'button', { name: 'Replace' } ).click();
-
-		// Match word specific number of times.
-		await expect(
-			page.getByText( 'item(s) replaced successfully.' )
-		).toBeVisible();
-		await expect(
-			page.getByText( 'item(s) replaced successfully.' )
-		).toHaveText( '3 item(s) replaced successfully.' );
-	} );
-
-	test( 'it matches text case for search and replace', async ( { page } ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
-
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
-
-		// Populate post title & content.
-		const postTitle = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'textbox', { name: 'Add title' } );
-		const postContent = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'document', { name: 'Empty block; start writing or' } );
-		const blockEditor = page
-			.locator( 'iframe[name="editor-canvas"]' )
-			.contentFrame()
-			.getByRole( 'button', { name: 'Add default block' } );
-
-		await expect( postTitle ).toBeVisible();
-		await postTitle.fill( 'Lorem ipsum dolor sit amet...' );
-
-		await expect( blockEditor ).toBeVisible();
-		await blockEditor.click();
-
-		await expect( postContent ).toBeVisible();
-		await postContent.fill(
-			'Lorem Ipsum dolor sit amet, consectetur adipiscing ipsum elit. Duis vestibulum at nulla vitae rutrum. Nunc purus nulla, tincidunt sed ipsum turpis in, ullamcorper commodo libero.'
-		);
-
-		// Click Plugin icon.
-		await expect(
-			page.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-		).toBeVisible();
-		await page
-			.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-			.click();
-
-		// Type in word in Search field.
-		await expect(
-			page.getByRole( 'textbox', { name: 'Search' } )
-		).toBeVisible();
-		await page.getByRole( 'textbox', { name: 'Search' } ).fill( 'Ipsum' );
-
-		// Type in word in Replace field.
-		await expect(
-			page.getByRole( 'textbox', { name: 'Replace' } )
-		).toBeVisible();
-		await page.getByRole( 'textbox', { name: 'Replace' } ).fill( 'hello' );
-
-		// Check the match case toggle.
-		const matchCase = page.getByRole( 'checkbox', { name: 'Match case' } );
-		await expect( matchCase ).toBeVisible();
-		if ( ! ( await matchCase.isChecked() ) ) {
-			await matchCase.click();
-		}
-
-		// Replace text.
-		await expect(
-			page.getByRole( 'button', { name: 'Replace' } )
-		).toBeVisible();
-		await page.getByRole( 'button', { name: 'Replace' } ).click();
-
-		// Match word specific number of times.
-		await expect(
-			page.getByText( 'item(s) replaced successfully.' )
-		).toBeVisible();
-		await expect(
-			page.getByText( 'item(s) replaced successfully.' )
-		).toHaveText( '1 item(s) replaced successfully.' );
-	} );
-
-	test( 'it toggles case matching and use regular expression by default', async ( {
-		page,
-	} ) => {
-		const closeIcon = page.getByRole( 'button', { name: 'Close' } );
-
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
-
-		await page.getByRole( 'link', { name: 'View Posts' } ).click();
-		await expect(
-			page.getByRole( 'link', { name: 'Posts', exact: true } )
-		).toBeVisible();
-
-		// Go to plugin options page.
-		await page
-			.getByRole( 'link', { name: 'Search & Replace for Block Editor' } )
-			.click();
-		await expect(
-			page.getByRole( 'heading', {
-				name: 'Search & Replace for Block Editor',
-			} )
-		).toBeVisible();
-
-		// Enable Case Matching.
-		const matchCase = page.locator( 'input[name="case_matching"]' );
-		await expect( matchCase ).toBeVisible();
-		if ( ! ( await matchCase.isChecked() ) ) {
-			await matchCase.click();
-		}
-
-		// Enable Regex Matching.
-		const matchRegex = page.locator( 'input[name="regex_matching"]' );
-		await expect( matchRegex ).toBeVisible();
-		if ( ! ( await matchRegex.isChecked() ) ) {
-			await matchRegex.click();
-		}
-
-		// Save plugin options.
-		await page.getByRole( 'button', { name: 'Save Changes' } ).click();
-
-		// Now create a new post.
-		await page.getByRole( 'link', { name: 'Posts', exact: true } ).click();
-		await page
-			.getByLabel( 'Main menu', { exact: true } )
-			.getByRole( 'link', { name: 'Add Post' } )
-			.click();
-
-		await expect( closeIcon ).toBeVisible();
-		await closeIcon.click();
-
-		// Click Plugin icon.
-		await expect(
-			page.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-		).toBeVisible();
-		await page
-			.getByRole( 'button', {
-				name: 'Search & Replace',
-			} )
-			.click();
-
-		// Ensure toggles are visible.
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Match case' } )
-		).toBeVisible();
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Use regular expression' } )
-		).toBeVisible();
-
-		// Ensure toggles are checked by default.
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Match case' } )
-		).toBeChecked();
-		await expect(
-			page.getByRole( 'checkbox', { name: 'Use regular expression' } )
-		).toBeChecked();
+		// TODO: We need to assert that we see the parsed columns.
 	} );
 } );
