7d211568db2bef04274010ff2954a747e89398c4
activist507 <zekeridanjuma304@gmail.com>
2026-04-13 12:34:59 +0100

    Test: update js unit test

--- tests/js/ImportButton.test.tsx
+++ tests/js/ImportButton.test.tsx
@@ -2,6 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import { ImportButton } from '../../src/components/All';
 import { handleUpload, handleImport } from '../../src/utils';
 
+
 import { useSelect } from '@wordpress/data';
 
 jest.mock( '@wordpress/data', () => ( {
---
+++ tests/js/Purge.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Purge } from '../../src/components/All';
+import { useDispatch } from '@wordpress/data';
+
+jest.mock( '@wordpress/data', () => ( {
+	useDispatch: jest.fn(),
+} ) );
+
+jest.mock( '@wordpress/i18n', () => ( {
+	__: ( text: string ) => text,
+} ) );
+
+jest.mock( '@wordpress/api-fetch', () => jest.fn().mockResolvedValue( {} ) );
+
+const mockUseDispatch = useDispatch as jest.Mock;
+global.sqlt = {
+	postTypes: [ 'student', 'teacher' ],
+};
+
+describe( 'Purge Component', () => {
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	it( 'renders the Purge CPT button', () => {
+		mockUseDispatch.mockReturnValue( {
+			setIsLoading: jest.fn(),
+			setSqlNotice: jest.fn(),
+		} );
+		render( <Purge /> );
+		(
+			expect( screen.getByRole( 'button', { name: 'Purge CPT' } ) ) as any
+		 ).toBeVisible();
+	} );
+
+	it( 'calls handlePurge when the Purge CPT button is clicked', () => {
+		const mockSetIsLoading = jest.fn();
+		const mockSetSqlNotice = jest.fn();
+
+		( useDispatch as jest.Mock ).mockReturnValue( {
+			setIsLoading: mockSetIsLoading,
+			setSqlNotice: mockSetSqlNotice,
+		} );
+
+		render( <Purge /> );
+		fireEvent.click( screen.getByRole( 'button', { name: 'Purge CPT' } ) );
+		expect( mockSetIsLoading ).toHaveBeenCalledWith( true );
+	} );
+} );
