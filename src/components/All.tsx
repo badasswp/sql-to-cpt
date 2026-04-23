af7222c508a6c22da9c6b01c251459e449dcdd37
badasswp <badasswpdev@gmail.com>
2026-04-23 15:44:32 +0000

    Build from release-1.4.0

--- src/components/All.tsx
+++
@@ -1,8 +0,0 @@
-import ImportButton from '../components/ImportButton';
-import Notice from '../components/Notice';
-import ProgressBar from '../components/ProgressBar';
-import Purge from '../components/Purge';
-import TableName from '../components/TableName';
-import TableColumns from '../components/TableColumns';
-
-export { ImportButton, Notice, ProgressBar, Purge, TableName, TableColumns };
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/sql-to-cpt',
+        'pretty_version' => 'dev-release-1.4.0-built',
+        'version' => 'dev-release-1.4.0-built',
+        'reference' => 'c01d76b632f73d82db7b132406096b162ef082a6',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/sql-to-cpt' => array(
+            'pretty_version' => 'dev-release-1.4.0-built',
+            'version' => 'dev-release-1.4.0-built',
+            'reference' => 'c01d76b632f73d82db7b132406096b162ef082a6',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
