f11498030197b543ec4da95d1eec7ec2889c31bb
badasswp <badasswpdev@gmail.com>
2026-05-06 20:49:58 +0000

    push to master-built

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-import '@testing-library/jest-dom';
-
-jest.mock( '@wordpress/components', () => ( {
-	Button: ( props ) => <button { ...props } />,
-} ) );
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/sql-to-cpt',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2a1469ef0d7e581d40de210f549759c9fb0c57e5',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/sql-to-cpt' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2a1469ef0d7e581d40de210f549759c9fb0c57e5',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
