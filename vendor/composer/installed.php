f84109dce142ad04d541189ea60f75d63054e3a2
badasswp <badasswpdev@gmail.com>
2026-05-22 19:09:49 +0000

    Build from release-1.4.3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/sql-to-cpt',
+        'pretty_version' => 'dev-release-1.4.3-built',
+        'version' => 'dev-release-1.4.3-built',
+        'reference' => '0f0719d165712b8181548e11f615d233fadf6792',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/sql-to-cpt' => array(
+            'pretty_version' => 'dev-release-1.4.3-built',
+            'version' => 'dev-release-1.4.3-built',
+            'reference' => '0f0719d165712b8181548e11f615d233fadf6792',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
